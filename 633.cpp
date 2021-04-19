7adb70089ce5b800f870b04e61d7b5a0c664bb95
AsterChang <80759991+asterchang96@users.noreply.github.com>
2021-04-19 23:18:52 +0800

    Add files via upload

---
+++ 633.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for(int a = sqrt(c); a >= 0; --a){
+            if((a*a) == c) return true;
+            int b = c - a*a ;
+            int t = sqrt(b);
+            if((t*t) == b) return true;
+        }
+        return false;  
+    }
+};
